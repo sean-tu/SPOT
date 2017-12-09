88b7c8f96e804a037ab925f215e33f6047bd31b7
Sean Reedy <sean2@knights.ucf.edu>
2017-12-09 01:42:03 -0500

    Tuned dilation/erosion, blurring, brightness, and thresholding.

--- video.py
+++ video.py
@@ -3,10 +3,14 @@ import cv2
 import math
 
 
+ANALYSIS = 1
+
+
 def main():
     print(cv2.__version__)
     # Open video capture source, 0 is webcam
-    source = '/home/sean/Desktop/laser2.avi'
+    # source = '/home/sean/Desktop/laser2.avi'
+    source = '/home/sean/PycharmProjects/SPOT/video_test.avi'
     cap = cv2.VideoCapture(source)
     width = int(cap.get(3))
     height = int(cap.get(4))
@@ -14,9 +18,9 @@ def main():
     print("Size: %d x %d\nOrigin: %d, %d" % (width, height, origin[0], origin[1]))
     t = -1
 
-    # Save video
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter('demo_vid.avi', fourcc, 20.0, (width, height))
+    # # Save video
+    # fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    # out = cv2.VideoWriter('demo_vid.avi', fourcc, 20.0, (width, height))
 
     while cap.isOpened():
         _, frame = cap.read(10)
@@ -33,20 +37,9 @@ def main():
 
         # frame = cv2.medianBlur(frame, 5)
         frame = cv2.GaussianBlur(frame, (5, 5), 0)
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # frame = cv2.medianBlur(frame, 3)
 
-        # Red range
-        target_min = np.array([170, 0, 0])
-        target_max = np.array([255, 255, 255])
-
-        # White range
-        target_lower_min = np.array([0, 0, 0])
-        target_lower_max = np.array([255, 12, 255])
-
-        # Create masks
-        mask1 = cv2.inRange(hsv, target_min, target_max)
-        mask2 = cv2.inRange(hsv, target_lower_min, target_lower_max)
-        mask = cv2.addWeighted(mask1, 1, mask2, 1, 0)
+        mask = mask_laser(frame)
 
         #  Draw origin and centerline
         # cv2.circle(frame, origin, 10, 255, 20)
@@ -78,9 +71,17 @@ def main():
         # out.write(frame)
         #
         mask = morph(mask)
-        contours(mask)
+        contours = contour_select(mask)
+        cv2.drawContours(frame, contours, -1, (255, 0, 0), 1)
+
+        print(len(contours))
+
+        if ANALYSIS and len(contours) != 1:
+            cv2.waitKey(0)
+
         masked = cv2.bitwise_and(frame, frame, mask=mask)
-        cv2.imshow('Masked', mask)
+        cv2.imshow('Masked', frame)
+        # cv2.imshow('Video', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -89,17 +90,57 @@ def main():
     cv2.destroyAllWindows()
 
 
-def contours(img):
-    conts = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-    print conts
+def contour_select(mask):
+    """From a binary image mask, select the contours that could be the laser."""
+    contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+    return contours
 
-def morph(img):
-    img = cv2.erode(img, None, iterations=2)
-    img = cv2.dilate(img, None, iterations=2)
-    return img
+
+def mask_laser(frame):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Red range
+    target_min = np.array([170, 0, 0])
+    target_max = np.array([255, 255, 255])
+
+    t_min = 200
+    t_max = 190
+    bgr_red_min = np.array([0, 0, t_min])
+    bgr_red_max = np.array([t_max, t_max, 255])
+
+    # White range
+    # target_lower_min = np.array([0, 0, 0])
+    # target_lower_max = np.array([255, 12, 255])
+    target_lower_min = np.array([0, 0, 0])
+    target_lower_max = np.array([255, 12, 255])
+
+    # Create masks
+    mask1 = cv2.inRange(frame, bgr_red_min, bgr_red_max)
+    # mask1 = cv2.inRange(hsv, target_min, target_max)
+    mask2 = cv2.inRange(hsv, target_lower_min, target_lower_max)
+    mask = cv2.addWeighted(mask1, 1, mask2, 1, 0)       # combine the two masks
+
+    return mask1
+
+
+def black_hat(img):
+    """The Black Hat operator isolates patches in an image that are brighter than their neighbors."""
+    bh = cv2.morphologyEx(img, cv2.MORPH_BLACKHAT)
+    (_, maxVal, _, maxLoc) = cv2.minMaxLoc(bh)
+    return bh
+
+
+def morph(mask):
+    """First erode the mask to remove noise, then dilate to fill in missing pixels.
+
+    This is akin to morphological closing."""
+    img = cv2.erode(mask, None, iterations=4)    # smooth/remove protrusions
+    mask = cv2.dilate(mask, None, iterations=2)   # smooth/fill concavities
+    return mask
 
 
 def detect(img):
+    """Returns the coordinates believed to be the center of the laser pointer, or None if not found."""
     img = cv2.GaussianBlur(img, (5, 5), 0)
     red = img[:, :, 2]                            # examine the red color channel of RGB image
     (_, maxVal, _, maxLoc) = cv2.minMaxLoc(red)     # locate the pixel with the highest intensity value
